0735cd3ed918bef5bff00924db96a0e284eddda9
Miroslav Rusin <miroslav.rusin@hotovo.org>
2014-10-06 13:36:12 +0200

    fixed npe bug, logging improved

--- src/main/java/org/mule/kicks/DepartmentProcessor.java
+++ src/main/java/org/mule/kicks/DepartmentProcessor.java
@@ -11,11 +11,12 @@ public class DepartmentProcessor implements Callable {
 	@Override
 	public Object onCall(MuleEventContext eventContext) throws Exception {
 		WorkerType worker = ((WorkerType) eventContext.getMessage().getPayload());
-		for (WorkerOrganizationMembershipDataType role : worker.getWorkerData().getOrganizationData().getWorkerOrganizationData()){
-			if (role.getOrganizationData().getOrganizationName().toLowerCase().contains("sales")) {
-				return true;
+		if (worker.getWorkerData().getOrganizationData() != null)
+			for (WorkerOrganizationMembershipDataType role : worker.getWorkerData().getOrganizationData().getWorkerOrganizationData()){
+				if (role.getOrganizationData().getOrganizationName().toLowerCase().contains("sales")) {
+					return true;
+				}
 			}
-		}
 		return false;
 	}
 
